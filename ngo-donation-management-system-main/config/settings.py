316a7ca3e96280bd793f15c975514cc6acc6cb9d
Sneha <sneha@gmail.com>
2026-05-13 10:49:15 +0530

    bug fix

--- ngo-donation-management-system-main/config/settings.py
+++ ngo-donation-management-system-main/config/settings.py
@@ -64,7 +64,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 # ===============================
 # URLs / WSGI
